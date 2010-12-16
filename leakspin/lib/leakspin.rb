9851108c3c31256dbc9efbc4fc3e4c50eedd7275
Alexandre Girard <alx.girard@gmail.com>
2010-12-16 21:59:05 +0100

    use class method to import db

--- leakspin/lib/leakspin.rb
+++ leakspin/lib/leakspin.rb
@@ -8,6 +8,42 @@ Bundler.require
 class LeakSpin < Sinatra::Application
   set :sessions, true
   set :root, APP_ROOT
+  
+  def self.fill_db_content
+    Dir.glob(File.join("..", "/cables/*")).each do |cable|
+      header = ""
+      content = ""
+      cable_id = File.basename(cable, ".txt")
+      has_been_classified = false
+
+      db_cable = Cable.create(:cable_id => cable_id)
+      
+      begin
+          file = File.new(cable, "r")
+          line_number = 1
+          while (line = file.gets)
+            if has_been_classified
+              if line == "\n"
+                db_cable.fragments << Fragment.create(:content => content, :type => :content, :line_number => line_number)
+                content = ""
+              else
+                content << line
+              end
+            elsif line =~ /^classified by/i
+              db_cable.fragments << Fragment.create(:content => header, :type => :header, :line_number => line_number)
+              has_been_classified = true
+            else
+              header << line
+            end
+            line_number += 1
+          end
+          db_cable.save
+          file.close
+      rescue => err
+          puts "Exception: #{err}"
+      end
+    end
+  end
 
   class Cable
     include DataMapper::Resource
@@ -24,6 +60,7 @@ class LeakSpin < Sinatra::Application
     property :id,  Serial
     property :content, Text
     property :type, Enum[:content, :header], :default => :content
+    property :line_number, Integer
 
     belongs_to :cable
     has n, :metadatas
@@ -35,7 +72,7 @@ class LeakSpin < Sinatra::Application
     property :id,  Serial
     property :name, Text
     property :value, Text
-    property :valid, Boolean, :default => false
+    property :validated, Boolean, :default => false
 
     belongs_to :fragment
     belongs_to :question
@@ -53,43 +90,17 @@ class LeakSpin < Sinatra::Application
   end
 
   DataMapper.setup(:default, 'postgres://localhost/leakspin')
-  DataMapper.finalize
-  DataMapper.auto_migrate!
+  
+  unless Cable.all.size > 0
+    DataMapper.finalize
+    DataMapper.auto_migrate!
+    LeakSpin.fill_db_content
+  end
   
   Question.create :text => "Select the subject", :help => "Select text and press enter"
 
   get '/update_db' do
-    Dir.glob(File.join("..", "/cables/*")).each do |cable|
-      header = ""
-      content = ""
-      cable_id = File.basename(cable, ".txt")
-      has_been_classified = false
-
-      db_cable = Cable.create(:cable_id => cable_id)
-
-      begin
-          file = File.new(cable, "r")
-          while (line = file.gets)
-            if has_been_classified
-              if line == "\n"
-                db_cable.fragments << Fragment.create(:content => content, :type => :content)
-                content = ""
-              else
-                content << line
-              end
-            elsif line =~ /^classified by/i
-              db_cable.fragments << Fragment.create(:content => header, :type => :header)
-              has_been_classified = true
-            else
-              header << line
-            end
-          end
-          db_cable.save
-          file.close
-      rescue => err
-          puts "Exception: #{err}"
-      end
-    end
+    LeakSpin.fill_db_content
   end
 
   get '/' do
