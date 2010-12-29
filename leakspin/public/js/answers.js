19d1fcf7f394031ab3c3310ecc2e4b81059316b3
Alexandre Girard <alx.girard@gmail.com>
2010-12-29 13:45:58 +0100

    correct var

--- leakspin/public/js/answers.js
+++ leakspin/public/js/answers.js
@@ -47,7 +47,7 @@ function loadAnswerForQuestion(question_id){
       });
       
       jQuery("#metadata_list").html(metadatas.join(""));
-      jQuery("#more_metadatas").html("Refresh (still " + question.progress.not_validated + ")");
+      jQuery("#more_metadatas").html("Refresh (still " + data.question.progress.not_validated + ")");
       loadFragment();
     }
   })
